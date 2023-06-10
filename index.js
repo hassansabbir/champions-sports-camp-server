bf99b492736bf7de431a282a8147eb7a2fcff557
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-06-10 08:15:45 +0600

    delete bookmark from server added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ymoa2u.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,6 +54,13 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await bookmarksCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/bookmarks", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookmarksCollection.deleteOne(query);
       res.send(result);
     });
 
