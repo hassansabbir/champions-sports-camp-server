66ddd8a1e765153d7e0f928600158cd6bd2323af
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-06-11 23:52:48 +0600

    updated

--- index.js
+++ index.js
@@ -109,6 +109,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/bookmarks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookmarksCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/bookmarks", async (req, res) => {
       const item = req.body;
       console.log(item);
