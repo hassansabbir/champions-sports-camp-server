955452f51bb1250fd4c58635d6dad12d518d8ed4
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-06-11 11:01:30 +0600

    classes status set from database

--- index.js
+++ index.js
@@ -39,6 +39,67 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/classes", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { instructorEmail: req.query.email };
+      }
+      const result = await toysCollection
+        .find(query)
+        // .sort({ price: req.query.sort === "true" ? 1 : -1 })
+        .toArray();
+
+      res.send(result);
+    });
+
+    app.get("/classes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/classes", async (req, res) => {
+      const result = await classesCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    app.patch("/classes/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          feedback: req.body.feedback,
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/classes/approve/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "approved",
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    app.patch("/classes/denied/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "denied",
+        },
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     //bookmarks collection
     app.get("/bookmarks", async (req, res) => {
       const email = req.query.email;
